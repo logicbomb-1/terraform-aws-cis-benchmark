47ed8d564f19f99700e6d50dc8655f79d159f0de
logicbomb-1 <57763597+logicbomb-1@users.noreply.github.com>
2019-12-19 23:47:32 +0530

    Create variables.tf

---
+++ secure_s3/variables.tf
@@ -0,0 +1,21 @@
+variable "force_destroy" {
+  type = (bool)
+}
+
+variable "log_transition_glacier_days" {
+  type = (number)
+}
+
+variable "user_bucket_name" {
+  type = (string)
+}
+
+variable "tag_bucket_name" {
+  type = (string)
+}
+
+variable "tag_bucket_env" {
+  type = (string)
+}
+
+
