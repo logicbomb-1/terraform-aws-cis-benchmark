76b08ac3c7fa9f2d493b71b2a3fcbf7dec118293
logicbomb-1 <57763597+logicbomb-1@users.noreply.github.com>
2019-12-19 23:49:18 +0530

    Create variables.tf

---
+++ vpc_flow/variables.tf
@@ -0,0 +1,42 @@
+variable "vpc_log_group_name" {
+  description = "The name of CloudWatch Logs group to which VPC Flow Logs are delivered."
+}
+
+variable "vpc_flow_logs_iam_role_arn" {
+  description = "The ARN of the IAM Role which will be used by VPC Flow Logs."
+}
+
+variable "vpc_log_retention_in_days" {
+  description = "Number of days to retain logs for. CIS recommends 365 days.  Possible values are: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653. Set to 0 to keep logs indefinitely."
+}
+
+variable "tags" {
+  description = "Specifies object tags key and value. This applies to all resources created by this module."
+  default = {
+    "Terraform" = true
+  }
+}
+
+variable "vpc_ids" {
+  description = "Enter VPC ID"
+  type = (string)
+}  
+  
+variable "vpc_ids1" {
+  default = [
+    {
+      name   = "test123"
+    },
+    {
+      name   = "test123"
+    }
+  ]
+}
+
+variable "vpc_ids2" {
+  description = "Map from availability zone to the number that should be used for each availability zone's subnet"
+  default     = {
+    "test123" = 1
+    "test1234" = 2
+  }
+}
