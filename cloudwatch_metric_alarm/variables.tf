e4bbef6fe2ca6c046dfa64867c426d105073f8b5
logicbomb-1 <57763597+logicbomb-1@users.noreply.github.com>
2019-12-19 23:40:47 +0530

    Create variables.tf

---
+++ cloudwatch_metric_alarm/variables.tf
@@ -0,0 +1,19 @@
+variable "sns_name" {
+  type = (string)
+}
+
+variable "alarm_namespace" {
+  type = (string)
+} 
+
+variable "cloudwatch_namespace" {
+  type = (string)
+}
+
+variable "cloudwatch_log_group_name" {
+  type = (string)
+}
+
+variable "retentions_days" {
+  type = (string)
+}  
